4cd7ca35989ea1f5f814f7843b5713c63c6b3a4a
alexandermalfait <alexandermalfait@gmail.com>
2012-01-15 19:11:29 +0100

    Search service updated

--- config/initializers/search_service.rb
+++ config/initializers/search_service.rb
@@ -2,10 +2,11 @@ require 'java'
 
 require 'jars/DPCSearcher-1.0.jar'
 require 'jars/dependency/postgresql-8.3-603.jdbc4.jar'
+require 'jars/dependency/guava-r09.jar'
 
 include_class 'be.alex.dpc.SearchService'
 
 if ENV['BOOT_SEARCH_ENGINE']
   SEARCH_SERVICE = SearchService.new
-  SEARCH_SERVICE.read_memory_dump "memory.dump"
+  SEARCH_SERVICE.read_data "data.txt"
 end
