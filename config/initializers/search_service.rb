453423210985cbff7c314f652bf2efd258624f69
Alexander Malfait <alexandermalfait@gmail.com>
2014-04-07 18:11:36 +0200

    Database info is now configurable

--- config/initializers/search_service.rb
+++ config/initializers/search_service.rb
@@ -5,10 +5,19 @@ require 'jars/dependency/postgresql-8.3-603.jdbc4.jar'
 require 'jars/dependency/guava-r09.jar'
 require 'jars/dependency/jregex-1.2_01.jar'
 
-include_class 'be.alex.dpc.SearchService'
+java_import 'be.alex.dpc.SearchService'
+java_import 'be.alex.dpc.DatabaseConfig'
 
 if ENV['BOOT_SEARCH_ENGINE']
+  database_config = DatabaseConfig.new
+  database_config.host = "localhost"
+  database_config.user = "postgres"
+  database_config.password = "olifant"
+  database_config.database = "dpc"
+
   SEARCH_SERVICE = SearchService.new
+
   SEARCH_SERVICE.progress_file_location = File.join(Rails.root, "public/current_progress.txt")
   SEARCH_SERVICE.data_location = "dumps"
+  SEARCH_SERVICE.database_config = database_config
 end
